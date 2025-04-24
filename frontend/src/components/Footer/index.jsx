eade802929a9887a62bf4af1e5a3e360f9bde670
Asger Finding <asger.finding@gmail.com>
2025-04-24 11:23:58 +0200

    feat: add icon to footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import { Typography } from "@material-tailwind/react";
+import icon from '../../assets/icon.svg';
 
 const YEAR = new Date().getFullYear();
 
@@ -17,7 +18,7 @@ export default function Footer() {
   return (
     <footer className="w-full">
       <div className="flex w-full flex-row flex-wrap items-center justify-center gap-x-12 gap-y-3 text-center md:justify-between">
-        <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/ct-assets/logo.png" alt="brand" className="w-8" />
+        <img src={icon} alt="brand" className="w-8" />
         <ul className="flex flex-wrap items-center gap-x-6 gap-y-2">
           {LINKS.map(({ title, href }, key) => (
             <li key={key}>
