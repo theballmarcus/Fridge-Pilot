0141d9067054746988981c71d832b2de52e47675
Asger Finding <asger.finding@gmail.com>
2025-04-24 10:10:12 +0200

    fix: slow tabler icons loading

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
     },
     resolve: {
         alias: {
+            // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
             '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
-        }
-    }
+        },
+    },
 })
