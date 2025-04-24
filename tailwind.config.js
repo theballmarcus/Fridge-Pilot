a3a220a7fab668fe7df7e959e88c9a49331df664
Asger Finding <asger.finding@gmail.com>
2025-04-24 09:36:00 +0200

    fix: don't use withMT object substitute in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-
-import { mtConfig } from "@material-tailwind/react";
+import withMT from '@material-tailwind/react/utils/withMT';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
         "./node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}",
-    ]
-}
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+});
