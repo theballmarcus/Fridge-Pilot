fb8325ee44f79a46b1637ba5b896d40ba3a61cee
Asger Finding <asger.finding@gmail.com>
2025-04-24 09:47:35 +0200

    chore: bump material-tailwind to v3 beta

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-import withMT from '@material-tailwind/react/utils/withMT';
+import { mtConfig } from "@material-tailwind/react";
 
 /** @type {import('tailwindcss').Config} */
-export default withMT({
+const config = {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
         "./node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}",
     ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-});
+  plugins: [mtConfig],
+};
+export default config;
