29ecd136a2586276b3e36c2a829f60c82c13fe31
Marcus <mcihme@gmail.com>
2025-04-29 21:07:28 +0200

    fix: removed unnecesarry tags

--- frontend/src/pages/Today/index.jsx
+++ frontend/src/pages/Today/index.jsx
@@ -5,9 +5,9 @@ import CalorieBuffer from '../../components/CalorieBuffer';
 export default function Today() {
     return <>
         <div className="flex flex-row items-start justify-between">
-            <div><KetoGuidelines></KetoGuidelines></div>
-            <div className="w-max"><MainPageProgress></MainPageProgress></div>
-            <div><CalorieBuffer></CalorieBuffer></div>
+            <div><KetoGuidelines /></div>
+            <div className="w-max"><MainPageProgress /></div>
+            <div><CalorieBuffer  /></div>
         </div>
     </>
 }
